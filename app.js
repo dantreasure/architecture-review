5e751f63f457eef97d62a702b72caee5c0b1cfee
Daniel Treasure <dbtreasure@gmail.com>
2015-10-05 21:10:43 -0600

    Invokes the users service in the user controller and places data on $scope

--- app.js
+++ app.js
@@ -28,11 +28,24 @@ app.factory('particulates', function($http) {
 	api.get = function() {
 		return $http.get('http://apis.is/particulates');
 	}
+	return api;
 })
 
-app.controller('main-ctrl', function($scope){
+app.controller('main-ctrl', function($scope, particulates){
 	$scope.message = "How?????";
+	particulates.get().then(function(response){
+		$scope.particulates = response.data.results[0];
+	})
 });
+
+//TODO: Inject the users service in to this controller and call the get
+// method and place the response object on the scope
 app.controller('users-ctrl', function($scope){
 	$scope.message = "Why?????";
 });
+
+
+
+
+
+
