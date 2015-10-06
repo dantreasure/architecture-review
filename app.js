1acd5da9934674234ec988f35c406976f6819f42
Daniel Breen <djbreen7@gmail.com>
2015-10-05 20:25:31 -0600

    Create Router

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var app = angular.module('app', ['ui.router']);
 
-app.controller('main-ctrl', function($scope){
-	$scope.message = 'Howdy!';
+app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
+	
+	$stateProvider
+	.state('home', {
+		url: '/',
+		templateUrl: 'home.html'		
+	});
 });
 
+app.controller('main-ctrl', function($scope) {
+	$scope.message = 'How???'
+})
