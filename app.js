832c970c904d400cb5dff1323f2a04698e05d199
Daniel Treasure <dbtreasure@gmail.com>
2015-10-05 20:30:38 -0600

    added ui-router

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var app = angular.module('app', ['ui.router']);
 
-app.controller('main-ctrl', function($scope){
-	$scope.message = 'Howdy!';
-});
+app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/');
 
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'home.html',
+			controller: 'main-ctrl'
+		})
+		.state('users', {
+			url: '/users',
+			templateUrl: 'users.html',
+			controller: 'users-ctrl'
+		})
+
+})
+app.controller('main-ctrl', function($scope){
+	$scope.message = "How?????";
+})
+app.controller('users-ctrl', function($scope){
+	$scope.message = "Why?????";
+})
